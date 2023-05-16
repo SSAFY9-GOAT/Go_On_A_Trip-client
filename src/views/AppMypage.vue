9c3423fc64090a69d6d2c81c0d7909cfeb3cf4f4
leeyr0412 <leeyr0412@naver.com>
2023-05-16 15:24:32 +0900

    🌊 refactor: 마이페이지 접근 페이지 수정

--- src/views/AppMypage.vue
+++ src/views/AppMypage.vue
@@ -1,37 +1,35 @@
 <template>
-  <div>
-      <b-container fluid>
-          <b-row>
-              <mypage-nav></mypage-nav>
-              마이페이지
-              <router-view></router-view>
-          </b-row>
-      </b-container>
-  </div>
+    <div>
+        <b-container fluid>
+            <b-row class="ms-auto me-auto">
+                <mypage-nav></mypage-nav>
+                <router-view></router-view>
+            </b-row>
+        </b-container>
+    </div>
 </template>
 
 <script>
-import MypageNav from "@/components/member/mypageNav.vue";
+import MypageNav from "@/components/member/MypageNav.vue";
 import axios from "axios";
 
 export default {
     name: "AppMypage",
-    data() {
-        return {
-            userInfo: null,
-        };
+    props: {
+        user: null,
     },
     components: {MypageNav},
-    methods:{
-        getUserInfo(){
+    methods: {
+        getUserInfo() {
             // user 정보 요청 api 주소
             const API_URL = `http://localhost:8080/mypage`;
             // axios 요청 (Spring Boot Rest API 참고)
             axios({
                 url: API_URL,
                 method: "get",
-                data:
-                this.$store.state.loginUser,
+                params: {
+                    loginId: this.$store.state.loginUser.id,
+                }
 
             }).then((res) => {
                 // res.data;
@@ -46,8 +44,8 @@ export default {
     },
     created() {
         this.$store.state.myPageState = "my-info";
-        console.log("[마이페이지] 로그인 한 유저={}",this.$store.state.loginUser)
-        this.getUserInfo();
+        // console.log("[마이페이지] 로그인 한 유저={}", this.$store.state.loginUser)
+        // this.getUserInfo();
     }
 }
 
