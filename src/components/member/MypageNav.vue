455d1e6cd23e21af4a3b3e9feefcba645c1d5b53
leeyr0412 <leeyr0412@naver.com>
2023-05-18 15:25:51 +0900

    🌊 refactor: 코드 정렬 및 비밀번호 수정 버튼 추가

--- src/components/member/MypageNav.vue
+++ src/components/member/MypageNav.vue
@@ -1,149 +1,88 @@
 <template>
-  <b-col class="col-3 ms-5">
-      <nav id="sidebarMenu" class=" d-md-block bg-light sidebar collapse">
-          <div class="position-sticky pt-3 sidebar-sticky">
-              <ul class="nav flex-column">
-                  <li class="nav-item">
-                      <router-link to="/mypage" v-if="this.$store.state.myPageState === 'my-info'" class="nav-link active" aria-current="page" href="">
-                          마이페이지
-                      </router-link>
-                      <router-link to="/mypage" v-else class="nav-link " aria-current="page" href="">
-                          마이페이지
-                      </router-link>
-<!--                      <c:if test="${currShow eq 'myPage'}">-->
-<!--                          <a class="nav-link active" aria-current="page" href="">-->
-<!--                      </c:if>-->
-<!--                      <c:if test="${currShow != 'myPage'}">-->
-<!--                          <a class="nav-link " aria-current="page" href="${root}/mypage">-->
-<!--                      </c:if>-->
-<!--                      <span data-feather="home" class="align-text-bottom"></span>-->
-<!--                      <i class="bi bi-person"></i>-->
-<!--                      마이페이지-->
-<!--                      </a>-->
-                  </li>
-<!--                  <li class="nav-item ">-->
-<!--                      <c:if test="${currShow eq 'myArticle'}">-->
-<!--                          <a class="nav-link active" aria-current="page" href="#">-->
-<!--                      </c:if>-->
-<!--                      <c:if test="${currShow != 'myArticle'}">-->
-<!--                          <a class="nav-link " aria-current="page" href="${root}/myArticle">-->
-<!--                      </c:if>-->
-<!--                      <span data-feather="file" class="align-text-bottom"></span>-->
-<!--                      내가 쓴 게시물-->
-<!--                      </a>-->
-<!--                  </li>-->
-<!--                  <li class="nav-item">-->
-<!--                      <c:if test="${currShow eq 'myHotplace'}">-->
-<!--                          <a class="nav-link active" aria-current="page" href="">-->
-<!--                      </c:if>-->
-<!--                      <c:if test="${currShow != 'myHotplace'}">-->
-<!--                          <a class="nav-link " aria-current="page"-->
-<!--                             href="${root}/myHotPlace">-->
-<!--                      </c:if>-->
-<!--                      <span data-feather="shopping-cart" class="align-text-bottom"></span>-->
-<!--                      내가 등록한 핫플레이스-->
-<!--                      </a>-->
-<!--                  </li>-->
-<!--                  <li class="nav-item">-->
-<!--                      <c:if test="${currShow eq 'myFavorite'}">-->
-<!--                          <a class="nav-link active" aria-current="page" href="">-->
-<!--                      </c:if>-->
-<!--                      <c:if test="${currShow != 'myFavorite'}">-->
-<!--                          <a class="nav-link " aria-current="page"-->
-<!--                             href="${root}/myFavorite">-->
-<!--                      </c:if>-->
-<!--                      <span data-feather="shopping-cart" class="align-text-bottom"></span>-->
-<!--                      내가 좋아요 누른 핫플레이스-->
-<!--                      </a>-->
-<!--                  </li>-->
-<!--                  <li class="nav-item mt-5">-->
-<!--                      <c:if test="${currShow eq 'modifyPw'}">-->
-<!--                          <a class="nav-link active" aria-current="page" href="">-->
-<!--                      </c:if>-->
-<!--                      <c:if test="${currShow != 'modifyPw'}">-->
-<!--                          <a class="nav-link " aria-current="page"-->
-<!--                             href="${root}/modifypw">-->
-<!--                      </c:if>-->
-<!--                      <span data-feather="home" class="align-text-bottom"></span>-->
-<!--                      <i class="bi bi-person"></i>-->
-<!--                      비밀번호 변경-->
-<!--                      </a>-->
-<!--                  </li>-->
-                  <li class="nav-item">
-                      <router-link to="/mypage/modify" v-if="this.$store.state.myPageState === 'modify'" class="nav-link active" aria-current="page" >
-                          <span data-feather="home" class="align-text-bottom"></span>
-                          <i class="bi bi-person"></i>
-                          정보 변경
-                      </router-link>
-                      <router-link to="/mypage/modify" v-else class="nav-link " aria-current="page" >
-                          <span data-feather="home" class="align-text-bottom"></span>
-                          <i class="bi bi-person"></i>
-                          정보 변경
-                      </router-link>
-<!--                      <c:if test="${currShow eq 'modifyNickname'}">-->
-<!--                          <a class="nav-link active" aria-current="page" href="">-->
-<!--                      </c:if>-->
-<!--                      <c:if test="${currShow != 'modifyNickname'}">-->
-<!--                          <a class="nav-link " aria-current="page"-->
-<!--                             href="${root}/modifynickname">-->
-<!--                      </c:if>-->
-<!--                      <span data-feather="home" class="align-text-bottom"></span>-->
-<!--                      <i class="bi bi-person"></i>-->
-<!--                      별명 변경-->
-<!--                      </a>-->
-                  </li>
-<!--                  <li class="nav-item">-->
-<!--                      <router-link to="/mypage" v-if="this.$store.state.myPageState === 'modify-email'" class="nav-link active" aria-current="page" >-->
-<!--                          <span data-feather="home" class="align-text-bottom"></span>-->
-<!--                          <i class="bi bi-person"></i>-->
-<!--                          이메일 변경-->
-<!--                      </router-link>-->
-<!--                      <router-link to="/mypage" v-else class="nav-link " aria-current="page" >-->
-<!--                          <span data-feather="home" class="align-text-bottom"></span>-->
-<!--                          <i class="bi bi-person"></i>-->
-<!--                          이메일 변경-->
-<!--                      </router-link>-->
-
-
-<!--                      <c:if test="${currShow eq 'modifyEmail'}">-->
-<!--                          <a class="nav-link active" aria-current="page" href="">-->
-<!--                      </c:if>-->
-<!--                      <c:if test="${currShow != 'modifyEmail'}">-->
-<!--                          <a class="nav-link " aria-current="page"-->
-<!--                             href="${root}/modifyemail">-->
-<!--                      </c:if>-->
-<!--                      <span data-feather="home" class="align-text-bottom"></span>-->
-<!--                      <i class="bi bi-person"></i>-->
-<!--                      이메일 변경-->
-<!--                      </a>-->
-<!--                  </li>-->
-<!--                  <li class="nav-item">-->
-<!--                      <router-link to="/mypage" v-if="this.$store.state.myPageState === 'modify-phone'" class="nav-link active" aria-current="page" >-->
-<!--                          <span data-feather="home" class="align-text-bottom"></span>-->
-<!--                          <i class="bi bi-person"></i>-->
-<!--                          전화번호 변경-->
-<!--                      </router-link>-->
-<!--                      <router-link to="/mypage" v-else class="nav-link " aria-current="page" >-->
-<!--                          <span data-feather="home" class="align-text-bottom"></span>-->
-<!--                          <i class="bi bi-person"></i>-->
-<!--                          전화번호 변경-->
-<!--                      </router-link>-->
-<!--                  </li>-->
-
-                  <li class="nav-item mt-5">
-                      <router-link to="/withdrawal" class="nav-link text-danger"  >
-                          <span data-feather="shopping-cart" class="align-text-bottom"></span>
-                          회원탈퇴
-                      </router-link>
-<!--                      <a class="nav-link text-danger" href="${root}/withdrawal">-->
-<!--                          <span data-feather="shopping-cart" class="align-text-bottom"></span>-->
-<!--                          회원탈퇴-->
-<!--                      </a>-->
-                  </li>
-              </ul>
-          </div>
-      </nav>
-  </b-col>
+    <b-col class="col-3 ms-5">
+        <nav id="sidebarMenu" class=" d-md-block bg-light sidebar collapse">
+            <div class="position-sticky pt-3 sidebar-sticky">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <router-link to="/mypage" v-if="this.$store.state.myPageState === 'my-info'"
+                                     class="nav-link active" aria-current="page" href="">
+                            마이페이지
+                        </router-link>
+                        <router-link to="/mypage" v-else class="nav-link " aria-current="page" href="">
+                            마이페이지
+                        </router-link>
+                    </li>
+                    <!--                  <li class="nav-item ">-->
+                    <!--                      <c:if test="${currShow eq 'myArticle'}">-->
+                    <!--                          <a class="nav-link active" aria-current="page" href="#">-->
+                    <!--                      </c:if>-->
+                    <!--                      <c:if test="${currShow != 'myArticle'}">-->
+                    <!--                          <a class="nav-link " aria-current="page" href="${root}/myArticle">-->
+                    <!--                      </c:if>-->
+                    <!--                      <span data-feather="file" class="align-text-bottom"></span>-->
+                    <!--                      내가 쓴 게시물-->
+                    <!--                      </a>-->
+                    <!--                  </li>-->
+                    <!--                  <li class="nav-item">-->
+                    <!--                      <c:if test="${currShow eq 'myHotplace'}">-->
+                    <!--                          <a class="nav-link active" aria-current="page" href="">-->
+                    <!--                      </c:if>-->
+                    <!--                      <c:if test="${currShow != 'myHotplace'}">-->
+                    <!--                          <a class="nav-link " aria-current="page"-->
+                    <!--                             href="${root}/myHotPlace">-->
+                    <!--                      </c:if>-->
+                    <!--                      <span data-feather="shopping-cart" class="align-text-bottom"></span>-->
+                    <!--                      내가 등록한 핫플레이스-->
+                    <!--                      </a>-->
+                    <!--                  </li>-->
+                    <!--                  <li class="nav-item">-->
+                    <!--                      <c:if test="${currShow eq 'myFavorite'}">-->
+                    <!--                          <a class="nav-link active" aria-current="page" href="">-->
+                    <!--                      </c:if>-->
+                    <!--                      <c:if test="${currShow != 'myFavorite'}">-->
+                    <!--                          <a class="nav-link " aria-current="page"-->
+                    <!--                             href="${root}/myFavorite">-->
+                    <!--                      </c:if>-->
+                    <!--                      <span data-feather="shopping-cart" class="align-text-bottom"></span>-->
+                    <!--                      내가 좋아요 누른 핫플레이스-->
+                    <!--                      </a>-->
+                    <!--                  </li>-->
+                    <li class="nav-item mt-5">
+                        <router-link to="/mypage/modifyPw" v-if="this.$store.state.myPageState === 'modifyPw'"
+                                     class="nav-link active" aria-current="page">
+                            <span data-feather="home" class="align-text-bottom"></span>
+                            <i class="bi bi-person"></i>
+                            비밀번호 변경
+                        </router-link>
+                        <router-link to="/mypage/modifyPw" v-else class="nav-link " aria-current="page">
+                            <span data-feather="home" class="align-text-bottom"></span>
+                            <i class="bi bi-person"></i>
+                            비밀번호 변경
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/mypage/modify" v-if="this.$store.state.myPageState === 'modify'"
+                                     class="nav-link active" aria-current="page">
+                            <span data-feather="home" class="align-text-bottom"></span>
+                            <i class="bi bi-person"></i>
+                            정보 변경
+                        </router-link>
+                        <router-link to="/mypage/modify" v-else class="nav-link " aria-current="page">
+                            <span data-feather="home" class="align-text-bottom"></span>
+                            <i class="bi bi-person"></i>
+                            정보 변경
+                        </router-link>
+                    </li>
+                    <li class="nav-item mt-5">
+                        <router-link to="/withdrawal" class="nav-link text-danger">
+                            <span data-feather="shopping-cart" class="align-text-bottom"></span>
+                            회원탈퇴
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </b-col>
 </template>
 
 <script>
