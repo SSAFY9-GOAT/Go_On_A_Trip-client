3068d8fb4658adb0c1ed7007742da0d6e327c7bc
qhfhdls12 <qhfhdls12@naver.com>
2023-05-23 17:53:04 +0900

    🌊 refactor: 게시판 작성 수정 삭제 오류 해결

--- src/views/AppBoard.vue
+++ src/views/AppBoard.vue
@@ -1,79 +1,87 @@
 <template>
-    <div>
-        <router-view @write="write" @modify="modify" @deleted="deleted"></router-view>
-    </div>
+  <div>
+    <router-view @write="write" @modify="modify" @deleted="deleted"></router-view>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    name: "AppBoard",
-    data() {
-        return {
-            article: null,
-        };
+  name: "AppBoard",
+  data() {
+    return {
+      article: null,
+    };
+  },
+  methods: {
+    write(article) {
+      const API_URL = `http://localhost:8080/articles/write`;
+      console.log(this.$store.state.memberStore.loginUser.id);
+      axios({
+        url: API_URL,
+        method: "POST",
+        data: {
+          title: article.title,
+          content: article.content,
+        },
+        params: {
+          loginUserId: this.$store.state.memberStore.loginUser.id,
+        },
+        headers: {
+          Authorization: sessionStorage.getItem("access-token"),
+          'access-token': sessionStorage.getItem("access-token"),
+        },
+      }).then((res) => {
+        console.log(res);
+        this.$router.push(`/articles`)
+      }).catch((err) => {
+        console.log(err);
+      });
     },
-    methods: {
-        write(article) {
-            const API_URL = `http://localhost:8080/articles/write`;
-            console.log(this.$store.state.memberStore.loginUser.id);
-            axios({
-                url: API_URL,
-                method: "POST",
-                data:{
-                    title: article.title,
-                    content: article.content,
-                },
-                params:{
-                    loginUserId: this.$store.state.memberStore.loginUser.id,
-                },
-                headers:{
-                    Authorization: sessionStorage.getItem("access-token"),
-                    'access-token': sessionStorage.getItem("access-token"),
-                },
-            }).then((res) => {
-                console.log(res);
-                this.$router.push(`/articles`)
-            }).catch((err) => {
-                console.log(err);
-            });
+    modify(article) {
+      let articleId = article.articleId;
+      const API_URL = `http://localhost:8080/articles/${articleId}/modify`;
+      console.log(article);
+      axios({
+        url: API_URL,
+        method: "POST",
+        data: {
+          title: article.title,
+          content: article.content,
         },
-        modify(article) {
-            let articleId = article.articleId;
-            const API_URL = `http://localhost:8080/articles/${articleId}/modify`;
-            console.log(article);
-            axios({
-                url: API_URL,
-                method: "POST",
-                data:{
-                    title: article.title,
-                    content: article.content,
-                },
-                params:{
-                    loginUserId: this.$store.state.memberStore.loginUser.id,
-                }
-            }).then(() => {
-                this.$router.push(`/articles/${articleId}`)
-            }).catch((err) => {
-                console.log(err);
-            });
+        params: {
+          loginUserId: this.$store.state.memberStore.loginUser.id,
         },
-        deleted(articleId) {
-            const API_URL = `http://localhost:8080/articles/${articleId}/delete`;
-            axios({
-                url: API_URL,
-                method: "POST",
-                params:{
-                    loginUserId: this.$store.state.memberStore.loginUser.id,
-                }
-            }).then(() => {
-                this.$router.push(`/articles`)
-            }).catch((err) => {
-                console.log(err);
-            });
-        }
+        headers: {
+          Authorization: sessionStorage.getItem("access-token"),
+          'access-token': sessionStorage.getItem("access-token"),
+        },
+      }).then(() => {
+        this.$router.push(`/articles/${articleId}`)
+      }).catch((err) => {
+        console.log(err);
+      });
+    },
+    deleted(articleId) {
+      const API_URL = `http://localhost:8080/articles/${articleId}/delete`;
+      axios({
+        url: API_URL,
+        method: "POST",
+        params: {
+          loginUserId: this.$store.state.memberStore.loginUser.id,
+        },
+        headers: {
+          Authorization: sessionStorage.getItem("access-token"),
+          'access-token': sessionStorage.getItem("access-token"),
+        },
+      }).then(() => {
+        this.$router.push(`/articles`)
+      }).catch((err) => {
+        console.log(err);
+      });
     }
+  }
 }
 </script>
 
