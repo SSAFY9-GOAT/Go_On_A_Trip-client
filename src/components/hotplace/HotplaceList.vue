1a094d8508ceba88888b97945c877780fbf8ad48
leeyr0412 <leeyr0412@naver.com>
2023-05-23 10:21:58 +0900

    🌞 feat: 핫플레이스 리스트 불러오기 구현 완료

--- src/components/hotplace/HotplaceList.vue
+++ src/components/hotplace/HotplaceList.vue
@@ -6,12 +6,13 @@
                 <div class="row g-3" method="get" action="${root}/hotPlace/list">
                     <input type="hidden" name="action" value="list">
                     <div class="col-auto">
-                        <input type="text" class="form-control" id="name" name="name" placeholder="작성자, 제목, 내용">
+                        <input type="text" v-model="name" class="form-control" id="name" name="name"
+                               placeholder="작성자, 제목, 내용">
                     </div>
                     <div class="col-auto">
-                        <select class="form-select" name="select">
-                            <option value="1" selected>최신등록순</option>
-                            <option value="2">인기순</option>
+                        <select class="form-select" name="select" type="number" v-model="sortCondition">
+                            <option value=1 selected>최신등록순</option>
+                            <option value=2>인기순</option>
                         </select>
                     </div>
                     <div class="col-auto">
@@ -20,48 +21,52 @@
                     </div>
                 </div>
             </div>
-            <!--            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-sm-start">-->
-            <!--                <div class="row row-cols-1 row-cols-md-4 g-4">-->
-            <!--                    &lt;!&ndash;                    <c:forEach items="${hotPlaces}" var="hotPlace">&ndash;&gt;-->
-            <!--                    <div class="col-4" style="width: 300px" v-for="hotplace in hotPlaceList" :key="hotplace.hotPlaceId">-->
-            <!--                        <div class="card h-100">-->
-            <!--                            <div class="position-relative">-->
-            <!--                                <img src="${root}/assets/img/userUpload/${hotPlace.storeFileName}"-->
-            <!--                                     class="card-img-top"-->
-            <!--                                     alt="..." style="width: 100%; height: 300px; object-fit:cover;">-->
+            <div
+                    class="d-flex flex-wrap align-items-center justify-content-center justify-content-sm-start">
+                <div class="row row-cols-1 row-cols-md-4 g-4">
+                    <!--                    <c:forEach items="${hotPlaces}" var="hotPlace">-->
+                    <div class="col-4" style="width: 300px" v-for="hotplace in hotPlaceList"
+                         :key="hotplace.hotPlaceId">
+                        <div class="card h-100">
+                            <div class="position-relative">
+<!--                               todo: 이미지 표시하기 -->
+                                <img :src="getImagePath(hotplace.storeFileName)"
+                                     class="card-img-top"
+                                     alt="..." style="width: 100%; height: 300px; object-fit:cover;">
 
-            <!--                            </div>-->
-            <!--                            <div class="card-body">-->
-            <!--                                <h5 class="card-title">${hotPlace.name}</h5>-->
-            <!--                                <p class="card-text text-truncate">${hotPlace.desc}</p>-->
-            <!--                                <div class="text-end">-->
-            <!--                                    조회수 : ${hotPlace.hit} | 작성자 : ${hotPlace.nickname}-->
-            <!--                                </div>-->
-            <!--                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">-->
-            <!--                                    <button type="button" class="btn btn-primary"-->
-            <!--                                            onclick="location.href='/hotPlace/${hotPlace.hotPlaceId}'">더보기-->
-            <!--                                    </button>-->
-            <!--                                    &lt;!&ndash;                                        <%&#45;&#45;&ndash;&gt;-->
-            <!--                                    &lt;!&ndash;                                        <button type="button" class="btn btn-primary" &#45;&#45;%>&ndash;&gt;-->
-            <!--                                    &lt;!&ndash;                                            <%&#45;&#45;&ndash;&gt;-->
-            <!--                                    &lt;!&ndash;                                            onclick="location.href='/hotPlace/mvedit&hotPlaceId=${hotPlace.hotPlaceId}'">수정&#45;&#45;%>&ndash;&gt;-->
-            <!--                                    &lt;!&ndash;                                            <%&#45;&#45;&ndash;&gt;-->
-            <!--                                    &lt;!&ndash;                                        </button>&ndash;&gt;-->
-            <!--                                    &lt;!&ndash;                                        &#45;&#45;%>&ndash;&gt;-->
-            <!--                                    <button type="button" class="btn btn-primary"-->
-            <!--                                            onclick="location.href='/likeHotPlace?hotPlaceId=${hotPlace.hotPlaceId}'">-->
-            <!--                                        담기-->
-            <!--                                    </button>-->
-            <!--                                </div>-->
-            <!--                            </div>-->
-            <!--                            <div class="card-footer">-->
-            <!--                                <small class="text-body-secondary">${hotPlace.createdDate}</small>-->
-            <!--                            </div>-->
-            <!--                        </div>-->
-            <!--                    </div>-->
-            <!--                    &lt;!&ndash;                    </c:forEach>&ndash;&gt;-->
-            <!--                </div>-->
-            <!--            </div>-->
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ hotplace.name }}</h5>
+                                <p class="card-text text-truncate">{{ hotplace.desc }}</p>
+                                <div class="text-end">
+                                    조회수 : {{ hotplace.hit }} <br> 작성자 : {{ hotplace.nickname }}
+                                </div>
+                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                    <router-link :to="{name: 'hotplace-detail', params: {hotPlaceId: hotplace.hotPlaceId}}"
+                                        type="button" class="btn btn-primary"
+                                           >더보기
+                                    </router-link>
+                                    <!--                                        <%---->
+                                    <!--                                        <button type="button" class="btn btn-primary" --%>-->
+                                    <!--                                            <%---->
+                                    <!--                                            onclick="location.href='/hotPlace/mvedit&hotPlaceId=${hotPlace.hotPlaceId}'">수정--%>-->
+                                    <!--                                            <%---->
+                                    <!--                                        </button>-->
+                                    <!--                                        --%>-->
+                                    <button type="button" class="btn btn-primary"
+                                            onclick="location.href='/likeHotPlace?hotPlaceId=${hotPlace.hotPlaceId}'">
+                                        담기
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-body-secondary">{{ hotplace.createdDate }}</small>
+                            </div>
+                        </div>
+                    </div>
+                    <!--                    </c:forEach>-->
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -74,33 +79,38 @@ export default {
     data() {
         return {
             hotPlaceList: [],
+            name: "",
+            sortCondition: 1,
         }
     },
     created() {
         this.loadData();
     },
     methods: {
+        getImagePath(fileName){
+            return '@/assets/img/userUpload/' + fileName; // 경로 추가
+            // return require(`@/assets/img/userUpload/${fileName}`);
+        },
         loadData() {
             // user 정보 요청 api 주소
             const API_URL = `http://localhost:8080/hotplaces`;
+            console.log("[핫플레이스] 리스트 요청")
             // axios 요청 (Spring Boot Rest API 참고)
             axios({
                 url: API_URL,
                 method: "get",
-                // params: {
-                //     loginId: this.$store.state.memberStore.loginUser.loginId,
-                // },
+                params: {
+                    name: this.name,
+                    sortCondition: parseInt(this.sortCondition),
+                },
                 headers: {
                     "Access-Control-Allow-Origin": "http://localhost:3000/",
                     "Access-Control-Allow-Headers": 'Authorization',
                     Authorization: sessionStorage.getItem("access-token"),
                 }
             }).then((res) => {
-                // res.data;
                 console.log("[핫플레이스] 리스트 정보 응답={}", res);
-                // if (res.data) {
-                //     this.userInfo = res.data;
-                // }
+                this.hotPlaceList = res.data;
             }).catch((err) => {
                 console.log(err);
             });
