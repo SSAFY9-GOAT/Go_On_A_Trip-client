6af1907cf7bfb4d47c01934ed21a983ae1138c85
leeyr0412 <leeyr0412@naver.com>
2023-05-23 17:48:35 +0900

    🌊 refactor: 메인페이지 수정

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        메인홈페이지
+        <info-view></info-view>
+        <trend-view></trend-view>
     </div>
 </template>
 
 <script>
+import TrendView from "@/components/index/TrendView.vue";
+import InfoView from "@/components/index/InfoView.vue";
+
 export default {
-    name: "IndexView"
+    name: "IndexView",
+    components: {InfoView, TrendView}
 }
 </script>
 
