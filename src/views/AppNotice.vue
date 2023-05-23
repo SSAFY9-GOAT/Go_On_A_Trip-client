c6a6f4af345154112c0ffe72b17cc0b6dab542e8
qhfhdls <79788971+DongHyun22@users.noreply.github.com>
2023-05-24 02:27:47 +0900

    🌞 feat: 공지사항 수정 삭제 구현

--- src/views/AppNotice.vue
+++ src/views/AppNotice.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <router-view @write="write" ></router-view>
+        <router-view @write="write" @modify="modify" @deleted="deleted"></router-view>
     </div>
 </template>
 
@@ -8,10 +8,10 @@
 import axios from "axios";
 
 export default {
-    name: "AppBoard",
+    name: "AppNotice",
     data() {
         return {
-            article: null,
+            notice: null,
         };
     },
     methods: {
@@ -40,6 +40,49 @@ export default {
                 console.log(err);
             });
         },
+        modify(notice) {
+            let noticeId = notice.id;
+            const API_URL = `http://localhost:8080/notice/${noticeId}/modify`;
+            console.log(notice);
+            axios({
+                url: API_URL,
+                method: "POST",
+                data: {
+                    title: notice.title,
+                    content: notice.content,
+                    top: notice.top,
+                },
+                params: {
+                    loginUserId: this.$store.state.memberStore.loginUser.id,
+                },
+                headers: {
+                    Authorization: sessionStorage.getItem("access-token"),
+                    'access-token': sessionStorage.getItem("access-token"),
+                },
+            }).then(() => {
+                this.$router.push(`/notice/${noticeId}`)
+            }).catch((err) => {
+                console.log(err);
+            });
+        },
+        deleted(noticeId) {
+            const API_URL = `http://localhost:8080/notice/${noticeId}/delete`;
+            axios({
+                url: API_URL,
+                method: "POST",
+                params: {
+                    loginUserId: this.$store.state.memberStore.loginUser.id,
+                },
+                headers: {
+                    Authorization: sessionStorage.getItem("access-token"),
+                    'access-token': sessionStorage.getItem("access-token"),
+                },
+            }).then(() => {
+                this.$router.push(`/notice`)
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
     }
 }
 </script>
