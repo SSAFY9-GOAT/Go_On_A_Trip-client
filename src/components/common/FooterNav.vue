dbb6a4f94c9bcf4db567e50011590d58612c403d
leeyr0412 <leeyr0412@naver.com>
2023-05-25 17:49:47 +0900

    🌈 style: footer 링크 추가

--- src/components/common/FooterNav.vue
+++ src/components/common/FooterNav.vue
@@ -2,10 +2,12 @@
     <div class="container">
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             <p class="col-md-4 mb-0 text-muted">&copy; SSAFY 광주5반</p>
+            <router-link :to="{name: 'index'}">
             <a href="${root}"
                class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                <img src="${root}/assets/img/logo/point.png" alt="" width="40"/>
+                <img :src="require('@/assets/img/logo/point.png')" alt="" width="40"/>
             </a>
+            </router-link>
             <ul class="nav col-md-4 justify-content-end">
                 <li class="nav-item">
                     <a href="#" class="nav-link px-2 text-muted">이용약관</a>
