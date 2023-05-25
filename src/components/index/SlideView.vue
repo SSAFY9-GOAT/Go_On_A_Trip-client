dccb2788375fb49b93646829342fd6e8dc5b7aa1
qhfhdls12 <qhfhdls12@naver.com>
2023-05-25 12:04:26 +0900

    🌞 feat: SlideView에 핫플 데이터 표시

--- src/components/index/SlideView.vue
+++ src/components/index/SlideView.vue
@@ -1,69 +1,196 @@
 <template>
-    <div class="wrapper" style="transition-timing-function: linear;">
-        <swiper
-            class="swiper"
-            :options="swiperOption">
-            <swiper-slide>Slide 1</swiper-slide>
-            <swiper-slide>Slide 2</swiper-slide>
-            <swiper-slide>Slide 3</swiper-slide>
-            <swiper-slide>Slide 4</swiper-slide>
-            <swiper-slide>Slide 5</swiper-slide>
-            <div
-                class="swiper-pagination"
-                slot="pagination"
-            >
-            </div>
-        </swiper>
-    </div>
+  <swiper
+      class="swiper"
+      :options="swiperOption">
+    <swiper-slide
+        v-for="hotplace in hotPlaceList" :key="hotplace.hotPlaceId">
+      <div class="card">
+        <div class="img" style="background-size:cover; object-fit: cover; height: 100%; width: 100%;"
+             :style="{backgroundImage: 'url(' + require(`@/assets/img/userUpload/${hotplace.storeFileName}`) + ')'}">
 
+        </div>
+        <div class="textBox">
+          <p class="text head">{{ hotplace.name }}</p>
+          <p class="text price">{{ hotplace.desc }}</p>
+        </div>
+      </div>
+
+
+      <!--              <div class="card h-100">-->
+      <!--                <div class="position-relative">-->
+      <!--                  <img :src="require(`@/assets/img/userUpload/${hotplace.storeFileName}`)"-->
+      <!--                       class="card-img-top"-->
+      <!--                       alt="..." style="width: 100%; height: 300px; object-fit:cover;">-->
+      <!--                </div>-->
+      <!--                <div class="card-body">-->
+      <!--                  <h5 class="card-title">{{ hotplace.name }}</h5>-->
+      <!--                  <p class="card-text text-truncate">{{ hotplace.desc }}</p>-->
+      <!--                  <div class="text-end">-->
+      <!--                    조회수 : {{ hotplace.hit }} <br> 작성자 : {{ hotplace.nickname }}-->
+      <!--                  </div>-->
+      <!--                  <div class="d-grid gap-2 d-md-flex justify-content-md-end">-->
+      <!--                  </div>-->
+      <!--                </div>-->
+      <!--                <div class="card-footer">-->
+      <!--                  <small class="text-body-secondary">{{ hotplace.createdDate }}</small>-->
+      <!--                </div>-->
+      <!--              </div>-->
+    </swiper-slide>
+  </swiper>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import {Swiper, SwiperSlide} from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
-
+import axios from "axios";
 export default {
-    components:	{
-        Swiper,
-        SwiperSlide
-    },
-    data() {
-        return {
-            swiperOption: {
-                slidesPerView: 5,
-                spaceBetween: 20,
-                freeMode: true,
-                loop: true,
-                autoplay: {
-                    delay: 0,
-                    disableOnInteraction: false,
-                    stopOnLastSlide: false,
-                },
-                speed: 5000,
-                observe: true,
-                observeParents: true,
-            },
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data() {
+    return {
+      hotPlaceList: [],
+      name: "",
+      sortCondition: 1,
+      swiperOption: {
+        slidesPerView: 7,
+        spaceBetween: 20,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+          delay: 8,
+          disableOnInteraction: false,
+          stopOnLastSlide: false,
+        },
+        speed: 6000,
+        observe: true,
+        observeParents: true,
+      },
+    }
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    loadData() {
+      const API_URL = `http://localhost:8080/hotplaces`;
+      console.log("[핫플레이스] 리스트 요청")
+      axios({
+        url: API_URL,
+        method: "get",
+        params: {
+          name: this.name,
+          sortCondition: parseInt(this.sortCondition),
+        },
+        headers: {
+          "Access-Control-Allow-Origin": "http://localhost:3000/",
+          "Access-Control-Allow-Headers": 'Authorization',
+          Authorization: sessionStorage.getItem("access-token"),
         }
-    },
+      }).then((res) => {
+        console.log("[핫플레이스] 리스트 정보 응답={}", res);
+        this.hotPlaceList = res.data;
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+  }
 }
 </script>
 
 
 <style scoped>
-.wrapper {
-    background: white;
 
-}
 .swiper {
-    height: 300px;
-    width: 100%;
+  height: 300px;
+  width: 100%;
+  background: white;
 
-    .swiper-slide {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-        font-weight: bold;
-    }
+  .swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-weight: bold;
+  }
 }
+
+.card {
+  width: 300px;
+  height: 285px;
+  background: whitesmoke;
+  border: 0;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  transition: 0.2s ease-in-out;
+}
+
+.img {
+  border-radius: 10px;
+  position: absolute;
+  transition: 0.2s ease-in-out;
+  z-index: 1;
+}
+
+.textBox {
+  opacity: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  transition: 0.2s ease-in-out;
+  z-index: 2;
+}
+
+.textBox > .text {
+  font-weight: bold;
+}
+
+.textBox > .head {
+  font-size: 20px;
+}
+
+.textBox > .price {
+  font-size: 17px;
+}
+
+.textBox > span {
+  font-size: 12px;
+  color: lightgrey;
+}
+
+.card:hover > .textBox {
+  opacity: 1;
+}
+
+.card:hover > .img {
+  filter: blur(7px);
+  animation: anim 3s infinite;
+}
+
+@keyframes anim {
+  0% {
+    transform: translateY(0);
+  }
+
+  50% {
+    transform: translateY(-20px);
+  }
+
+  100% {
+    transform: translateY(0);
+  }
+}
+
+.card:hover {
+  transform: scale(1.04) rotate(-1deg);
+}
+
+
 </style>
