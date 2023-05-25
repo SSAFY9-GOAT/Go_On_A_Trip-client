8b28366cd89c012770e44c0a5da23545a06d53e7
leeyr0412 <leeyr0412@naver.com>
2023-05-25 17:51:21 +0900

    🌊 refactor: 코드정리

--- src/components/index/SlideView.vue
+++ src/components/index/SlideView.vue
@@ -8,94 +8,75 @@
         <div class="img" style="background-size:cover; object-fit: cover; height: 100%; width: 100%;"
              :style="{backgroundImage: 'url(' + require(`@/assets/img/userUpload/${hotplace.storeFileName}`) + ')'}">
 
-        </div>
-        <div class="textBox">
-          <p class="text head">{{ hotplace.name }}</p>
-          <p class="text price">{{ hotplace.desc }}</p>
-        </div>
-      </div>
+                </div>
+                <div class="textBox">
+                    <p class="text head">{{ hotplace.name }}</p>
+                    <p class="text price">{{ hotplace.desc }}</p>
+                </div>
+            </div>
+        </swiper-slide>
+    </swiper>
 
-
-      <!--              <div class="card h-100">-->
-      <!--                <div class="position-relative">-->
-      <!--                  <img :src="require(`@/assets/img/userUpload/${hotplace.storeFileName}`)"-->
-      <!--                       class="card-img-top"-->
-      <!--                       alt="..." style="width: 100%; height: 300px; object-fit:cover;">-->
-      <!--                </div>-->
-      <!--                <div class="card-body">-->
-      <!--                  <h5 class="card-title">{{ hotplace.name }}</h5>-->
-      <!--                  <p class="card-text text-truncate">{{ hotplace.desc }}</p>-->
-      <!--                  <div class="text-end">-->
-      <!--                    조회수 : {{ hotplace.hit }} <br> 작성자 : {{ hotplace.nickname }}-->
-      <!--                  </div>-->
-      <!--                  <div class="d-grid gap-2 d-md-flex justify-content-md-end">-->
-      <!--                  </div>-->
-      <!--                </div>-->
-      <!--                <div class="card-footer">-->
-      <!--                  <small class="text-body-secondary">{{ hotplace.createdDate }}</small>-->
-      <!--                </div>-->
-      <!--              </div>-->
-    </swiper-slide>
-  </swiper>
 </template>
 
 <script>
 import {Swiper, SwiperSlide} from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
 import axios from "axios";
+
 export default {
-  components: {
-    Swiper,
-    SwiperSlide
-  },
-  data() {
-    return {
-      hotPlaceList: [],
-      name: "",
-      sortCondition: 1,
-      swiperOption: {
-        slidesPerView: 7,
-        spaceBetween: 20,
-        freeMode: true,
-        loop: true,
-        autoplay: {
-          delay: 8,
-          disableOnInteraction: false,
-          stopOnLastSlide: false,
-        },
-        speed: 6000,
-        observe: true,
-        observeParents: true,
-      },
-    }
-  },
-  created() {
-    this.loadData();
-  },
-  methods: {
-    loadData() {
-      const API_URL = `http://localhost:8080/hotplaces`;
-      console.log("[핫플레이스] 리스트 요청")
-      axios({
-        url: API_URL,
-        method: "get",
-        params: {
-          name: this.name,
-          sortCondition: parseInt(this.sortCondition),
-        },
-        headers: {
-          "Access-Control-Allow-Origin": "http://localhost:3000/",
-          "Access-Control-Allow-Headers": 'Authorization',
-          Authorization: sessionStorage.getItem("access-token"),
+    components: {
+        Swiper,
+        SwiperSlide
+    },
+    data() {
+        return {
+            hotPlaceList: [],
+            name: "",
+            sortCondition: 1,
+            swiperOption: {
+                slidesPerView: 7,
+                spaceBetween: 20,
+                freeMode: true,
+                loop: true,
+                autoplay: {
+                    delay: 8,
+                    disableOnInteraction: false,
+                    stopOnLastSlide: false,
+                },
+                speed: 6000,
+                observe: true,
+                observeParents: true,
+            },
         }
-      }).then((res) => {
-        console.log("[핫플레이스] 리스트 정보 응답={}", res);
-        this.hotPlaceList = res.data;
-      }).catch((err) => {
-        console.log(err);
-      });
+    },
+    created() {
+        this.loadData();
+    },
+    methods: {
+        loadData() {
+            const API_URL = `http://localhost:8080/hotplaces`;
+            console.log("[핫플레이스] 리스트 요청")
+            axios({
+                url: API_URL,
+                method: "get",
+                params: {
+                    name: this.name,
+                    sortCondition: parseInt(this.sortCondition),
+                },
+                headers: {
+                    "Access-Control-Allow-Origin": "http://localhost:3000/",
+                    "Access-Control-Allow-Headers": 'Authorization',
+                    Authorization: sessionStorage.getItem("access-token"),
+                }
+            }).then((res) => {
+                console.log("[핫플레이스] 리스트 정보 응답={}", res);
+                this.hotPlaceList = res.data;
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
     }
-  }
 }
 </script>
 
@@ -103,57 +84,57 @@ export default {
 <style scoped>
 
 .swiper {
-  height: 300px;
-  width: 100%;
-  background: white;
+    height: 300px;
+    width: 100%;
+    background: white;
 
-  .swiper-slide {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    font-weight: bold;
-  }
+    .swiper-slide {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-weight: bold;
+    }
 }
 
 .card {
-  width: 300px;
-  height: 285px;
-  background: whitesmoke;
-  border: 0;
-  border-radius: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: white;
-  transition: 0.2s ease-in-out;
+    width: 300px;
+    height: 285px;
+    background: whitesmoke;
+    border: 0;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    transition: 0.2s ease-in-out;
 }
 
 .img {
-  border-radius: 10px;
-  position: absolute;
-  transition: 0.2s ease-in-out;
-  z-index: 1;
+    border-radius: 10px;
+    position: absolute;
+    transition: 0.2s ease-in-out;
+    z-index: 1;
 }
 
 .textBox {
-  opacity: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 15px;
-  transition: 0.2s ease-in-out;
-  z-index: 2;
+    opacity: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+    transition: 0.2s ease-in-out;
+    z-index: 2;
 }
 
 .textBox > .text {
-  font-weight: bold;
+    font-weight: bold;
 }
 
 .textBox > .head {
-  font-size: 20px;
+    font-size: 20px;
 }
 
 .textBox > .price {
